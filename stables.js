01b7d8d6adea6c9c4ee0048984e16e213fa4a2f9
Carson <cferguson50@my.bcit.ca>
2025-10-09 17:23:02 -0700

    added instructions for stables #2 homework

--- stables.js
+++ stables.js
@@ -211,3 +211,16 @@ console.log(payMeLateFees());
 //         ) _.'\`-....'                   `-....'
 //        ('`    \
 //         `-.___/
+//
+//
+//
+//------------------------- Growing our business (Lab #2, Week 6) -------------------------//
+
+// Create a variable that stores your total number of stalls.
+// Create a function that logs out how many stalls are available, given how many horses you have in your stable.
+
+// Create a function that logs out how much will an individual horse will owe if rent is
+// paid late. Invoke the function.
+
+// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
+// the return value outside of the function.
